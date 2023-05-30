13d1a951fb01b6facf0f2a35933b5cb9095a3119
Suleyman Baris Eser <suleymanbariseser@gmail.com>
2023-05-30 21:41:59 +0300

    create icon button

--- native/components/ui/icon-button.tsx
+++ native/components/ui/icon-button.tsx
@@ -1,15 +1,27 @@
+import type { ColorTokens } from 'tamagui';
 import { StackProps } from 'tamagui';
 
 import { BaseButton } from './button';
 
 interface Props extends StackProps {
-  children?: React.ReactNode;
+  icon: ({ color }: { color: ColorTokens }) => React.ReactNode;
 }
 
-export default function IconButton({ children, ...props }: Props) {
+export default function IconButton({ children, icon, ...props }: Props) {
   return (
-    <BaseButton px="$0" py="$0" width="$8" height="$8" {...props}>
-      {children}
+    <BaseButton
+      px="$0"
+      py="$0"
+      w={40}
+      h={40}
+      ai="center"
+      jc="center"
+      bg="#ffffff10"
+      pressStyle={{
+        bg: '#ffffff20',
+      }}
+      {...props}>
+      {icon({ color: '$textPrimary' })}
     </BaseButton>
   );
 }
