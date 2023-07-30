9e255eca0125762a6a380fe2c8bbf0a699b60e31
Suleyman Baris Eser <suleymanbariseser@gmail.com>
2023-07-30 12:57:15 +0300

    add refresh to network list

--- apps/client/app/(main)/index.tsx
+++ apps/client/app/(main)/index.tsx
@@ -1,4 +1,6 @@
-import { FlatList } from 'react-native';
+import { useToastController } from '@tamagui/toast';
+import { useState } from 'react';
+import { FlatList, RefreshControl } from 'react-native';
 import { Stack } from 'tamagui';
 
 import HomeCard from '@/components/home/home-card';
@@ -6,12 +8,34 @@ import HomeTabs from '@/components/home/home-tabs';
 import { trpc } from '@/lib/trpc';
 
 const Home = () => {
-  const [data] = trpc.activity.networkList.useSuspenseQuery();
+  const [data, { refetch }] = trpc.activity.networkList.useSuspenseQuery();
+
+  const toast = useToastController();
+  const [refreshing, setRefreshing] = useState(false);
+
+  const handleRefresh = async () => {
+    try {
+      setRefreshing(true);
+
+      await refetch();
+
+      setRefreshing(false);
+    } catch (error) {
+      toast.show(error.message, {
+        variant: 'error',
+      });
+    }
+  };
 
   return (
     <Stack pos="relative" f={1} px="$2">
       <FlatList
         data={data}
+        refreshing={refreshing}
+        onRefresh={handleRefresh}
+        refreshControl={
+          <RefreshControl tintColor="white" refreshing={refreshing} onRefresh={handleRefresh} />
+        }
         renderItem={({ item }) => (
           <HomeCard
             admin={{
