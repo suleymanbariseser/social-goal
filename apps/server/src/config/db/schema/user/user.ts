68ca34ccda1653519ce82d78932bd5f503ca6e43
Suleyman Baris Eser <suleymanbariseser@gmail.com>
2023-08-27 22:17:05 +0300

    fix follower and following counts

--- apps/server/src/config/db/schema/user/user.ts
+++ apps/server/src/config/db/schema/user/user.ts
@@ -29,8 +29,10 @@ export const userRelations = relations(users, ({ many }) => ({
   goals: many(goals),
   activities: many(activities),
   socialLinks: many(userSocialLinks),
-  followers: many(userRelationships, { relationName: 'follower' }),
-  followings: many(userRelationships, { relationName: 'following' }),
+
+  // ? the relation name is reverse because the followers are people who following me
+  followers: many(userRelationships, { relationName: 'following' }),
+  followings: many(userRelationships, { relationName: 'follower' }),
 }));
 
 export type User = InferModel<typeof users>;
