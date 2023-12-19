77ed601924d4d3ccd339ed8aa0c92192c9113f8b
suleymanbariseser <suleymanbariseser@gmail.com>
2023-12-19 21:31:36 +0300

    update comment input layout

--- apps/client/components/activity/activity-comment/activity-comment-input/activity-comment-input.tsx
+++ apps/client/components/activity/activity-comment/activity-comment-input/activity-comment-input.tsx
@@ -1,11 +1,20 @@
-import { Stack } from 'tamagui';
-
-import { Text } from '@/components/ui/text';
+import { BaseInput } from '@/components/ui/form/input';
+import { SafeAreaView } from '@/components/ui/safe-area-view';
 
 export const ActivityCommentInput = () => {
   return (
-    <Stack pos="absolute" b={0} l={0} r={0} btw={1} btc="$borderColor">
-      <Text>ActivityCommentInput</Text>
-    </Stack>
+    <SafeAreaView
+      edges={['bottom']}
+      pos="absolute"
+      b={0}
+      l={0}
+      r={0}
+      bg="$backgroundMain"
+      px="$2"
+      pt="$4"
+      btw={1}
+      btc="$borderColor">
+      <BaseInput placeholder="Leave a comment..." />
+    </SafeAreaView>
   );
 };
