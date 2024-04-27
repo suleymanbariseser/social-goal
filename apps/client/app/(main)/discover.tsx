c807db819dae4f3c3789e6c04613e1fc659faccb
suleymanbariseser <suleymanbariseser@gmail.com>
2024-04-27 16:34:05 +0300

    create discover tabs

--- apps/client/app/(main)/discover.tsx
+++ apps/client/app/(main)/discover.tsx
@@ -1,13 +1,13 @@
 import { Stack } from 'tamagui';
 
-import { ActivityList } from '@/components/activity';
 import { AppTabs } from '@/components/app-tabs';
 import { DiscoverSearchOverlay } from '@/components/discover/discover-search/discover-search-overlay';
+import { DiscoverTabs } from '@/components/discover/discover-tabs/discover-tabs';
 
 const Discover = () => {
   return (
     <Stack f={1} px="$2">
-      <ActivityList onPress={console.log} onPressAvatar={console.log} />
+      <DiscoverTabs />
       <DiscoverSearchOverlay />
       <AppTabs activeTab="discover" />
     </Stack>
---
+++ apps/client/components/discover/discover-tabs/discover-tabs.tsx
@@ -0,0 +1,38 @@
+import { ScrollView, Stack, Tabs } from 'tamagui';
+
+import { ActivityList } from '@/components/activity';
+import { Text } from '@/components/ui/text';
+
+export const DiscoverTabs = () => {
+  return (
+    <Tabs defaultValue="accounts" fd="column" f={1}>
+      <Stack w="100%">
+        <ScrollView fg={1} horizontal showsHorizontalScrollIndicator={false}>
+          <Tabs.List>
+            <Tabs.Tab value="accounts">
+              <Text variant="subtitle2">Accounts</Text>
+            </Tabs.Tab>
+            <Tabs.Tab value="activities">
+              <Text variant="subtitle2">Activities</Text>
+            </Tabs.Tab>
+            <Tabs.Tab value="goals">
+              <Text variant="subtitle2">Goals</Text>
+            </Tabs.Tab>
+          </Tabs.List>
+        </ScrollView>
+      </Stack>
+
+      <Stack fg={1}>
+        <Tabs.Content value="accounts" fg={1}>
+          <Text>Accounts</Text>
+        </Tabs.Content>
+        <Tabs.Content value="activities">
+          <ActivityList onPress={console.log} onPressAvatar={console.log} />
+        </Tabs.Content>
+        <Tabs.Content value="goals">
+          <Text>Goals</Text>
+        </Tabs.Content>
+      </Stack>
+    </Tabs>
+  );
+};
